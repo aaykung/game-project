aa9aa25f6bd026c41c4f8dc37d5e6ad59b328c64
aaykung <68363308+aaykung@users.noreply.github.com>
2020-10-22 07:04:16 +0700

    บันทึกที่ 2

--- login.php
+++ login.php
@@ -4,11 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>login Page</title>
     
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Hello php<h1>
+    <div class="header">
+        <h2>Login</h2>
+    </div>
+
+<form action="register_db.php">
+    <div class="input-group">
+        <label for="username">Username</label>
+        <input type="text" name="username">
+    </div>
+    <div class="input-group">
+        <label for="password">Password</label>
+        <input type="password" name="password">
+    </div>
+    <div class="input-group">
+        <button type="submit" name="login_user" class="btn">Login</button>
+    <p>Not yet  a member? <a href="register.php">Sign up </a></p>
+</form>
 </body>
 </html>
--- sever.php
+++ sever.php
@@ -10,7 +10,7 @@
     //check connection
     if (!$conn) {
         die("connection failed". mysqli_connect_error());
-    } else {
-        echo "Connected successfully";
-    }
+    } else 
+     
+    
 ?>
